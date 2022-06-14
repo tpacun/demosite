8b54c1eb7c272913859ad7b67de86741fccc9681
theopacun <106777221+theopacun@users.noreply.github.com>
2022-06-14 16:47:50 -0400

    adding some stuff

--- assets/javascript/tooltipContent.js
+++ assets/javascript/tooltipContent.js
@@ -2,6 +2,10 @@ function addContent(query, content) {
     document.querySelector(query).setAttribute('data-tooltip', content)
 }
 
+window.onload = () => {
+
 addContent('.ChooseSeatsHeading', 'Editable under Settings > System Setup > Custom Website Messages.')
 
 addContent('.SeatingAreaOptionalInstructions', 'Editable under Settings > System Setup > Custom Website Messages.')
+
+}
